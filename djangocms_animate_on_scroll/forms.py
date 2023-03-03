903f64219b5e00c8ccf83569aef129fe750c691d
Fabs <fabien.michel@hespul.org>
2023-03-03 16:48:09 +0100

    feat: display default in widget value for ease and mirror

--- djangocms_animate_on_scroll/forms.py
+++ djangocms_animate_on_scroll/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 from django.forms.models import ModelForm
+from django.utils.translation import gettext_lazy as _
 
 from .consts import AOS_ANCHOR_PLACEMENT, AOS_ANIMATIONS, AOS_EASING
 from .settings import get_aos_init_params
+
+
+def choices_with_default_value(choices, default_value):
+    if isinstance(default_value, bool):
+        default_value = _("Yes") if default_value else _("No")
+    first_choice_str = f'{_("Default")} ({default_value})'
+    first_choice = (choices[0][0], first_choice_str)
+    return (first_choice, *choices[1:])
 
 
 class AnimateOnScroll_Element_Form(ModelForm):
@@ -18,3 +27,11 @@ class AnimateOnScroll_Element_Form(ModelForm):
         self.fields["aos_offset"].widget.attrs["placeholder"] = aoe_init_params["offset"]
         self.fields["aos_duration"].widget.attrs["placeholder"] = aoe_init_params["duration"]
         self.fields["aos_delay"].widget.attrs["placeholder"] = aoe_init_params["delay"]
+
+        self.fields["aos_once"].widget.choices = choices_with_default_value(
+            self.fields["aos_once"].widget.choices, aoe_init_params["once"]
+        )
+
+        self.fields["aos_mirror"].widget.choices = choices_with_default_value(
+            self.fields["aos_mirror"].widget.choices, aoe_init_params["mirror"]
+        )
